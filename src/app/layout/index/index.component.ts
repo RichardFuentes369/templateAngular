65ab5c623a4c66f3772f0200644b030d6c7128cf
Javier Pc <javierbaron6@mail.com>
2024-03-21 02:38:12 -0500

    ajustes

--- src/app/layout/index/index.component.ts
+++ src/app/layout/index/index.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, HostListener, OnInit  } from '@angular/core';
 
 @Component({
   selector: 'app-index',
@@ -6,5 +6,46 @@ import { Component } from '@angular/core';
   styleUrl: './index.component.scss'
 })
 export class IndexComponent {
+  constructor(private renderer: Renderer2) {}
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any) {
+    this.responsiveMenu()
+  }
+
+  ngOnInit(event:any) {
+    this.responsiveMenu()
+  }
+
+  responsiveMenu(){
+    if(window.innerWidth <= 991){
+      const elements = document.querySelectorAll('.divider');
+      elements.forEach(element => {
+        this.renderer.addClass(element, 'd-none');
+      });
+
+      const container = document.querySelectorAll('.navbarPersonalizado');
+      container.forEach(element => {
+        this.renderer.removeClass(element, 'row');
+      });
+    }else{
+      const elements = document.querySelectorAll('.divider');
+      elements.forEach(element => {
+        this.renderer.removeClass(element, 'd-none');
+      });
+
+      const container = document.querySelectorAll('.navbarPersonalizado');
+      container.forEach(element => {
+        this.renderer.addClass(element, 'row');
+      });
+    }
+  }
+
+  minimizarSliderbar: boolean = false;
+
+  mostrarMenuLateral(){
+    this.minimizarSliderbar = !this.minimizarSliderbar
+    console.log(this.minimizarSliderbar)
+  }
 
 }
