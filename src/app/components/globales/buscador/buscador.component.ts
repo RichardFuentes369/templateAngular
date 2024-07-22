3fc97dae40a61827402893bcaf429f40eca8c92d
Javier Pc <javierbaron6@mail.com>
2024-07-22 15:49:27 -0500

    ajustes

---
+++ src/app/components/globales/buscador/buscador.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-globales-buscador',
+  standalone: true,
+  templateUrl: './buscador.component.html',
+  styleUrl: './buscador.component.scss'
+})
+export class BuscadorComponent {
+
+}
